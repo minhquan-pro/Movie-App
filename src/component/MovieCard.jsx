2e2f268933f5effe7d177e26506dd4ffced0a991
Minh Quan <minhquan02227@gmail.com>
2025-07-27 20:28:56 +0700

    Add Link routing for TVShow

--- src/component/MovieCard.jsx
+++ src/component/MovieCard.jsx
@@ -4,34 +4,35 @@ import Image from "./Image";
 
 const MovieCard = ({ media, tabActive }) => {
   return (
-    <Link
-      to={`/movie/${media.id}`}
-      className="relative cursor-pointer rounded-md border border-slate-500"
-      onClick={() => {
-        window.scrollTo(0, 0);
-      }}
-    >
-      <Image
-        src={`https://image.tmdb.org/t/p/w500/${media.poster_path}`}
-        alt
-        className="w-full rounded-md"
-        width={292}
-        height={437}
-      />
+    <div className="relative cursor-pointer rounded-md border border-slate-500 text-[1.2vw]">
+      <Link
+        to={`${media.media_type === "tv" ? `/tv/${media.id}` : `/movie/${media.id}`} `}
+        onClick={() => {
+          window.scrollTo(0, 0);
+        }}
+      >
+        <Image
+          src={`https://image.tmdb.org/t/p/w500/${media.poster_path}`}
+          alt
+          className="w-full rounded-md"
+          width={292}
+          height={437}
+        />
 
-      <div className="relative top-[-5%] px-4 py-2 lg:top-[-6%]">
-        <CircularProgressBar voteAverage={media.vote_average} />
-        <p className="mt-4 font-bold">{media.title || media.name}</p>
-        <p className="mt-1">{media.release_date || media.first_air_date}</p>
-      </div>
-      {tabActive === "tv" || media.media_type === "tv" ? (
-        <div className="absolute bottom-[2%] right-2 inline-block rounded bg-white px-2 py-1 font-bold text-black shadow-sm">
-          <p>TV SHOW</p>
+        <div className="relative top-[-5%] px-4 py-2 lg:top-[-6%]">
+          <CircularProgressBar voteAverage={media.vote_average} />
+          <p className="mt-4 font-bold">{media.title || media.name}</p>
+          <p className="mt-1">{media.release_date || media.first_air_date}</p>
         </div>
-      ) : (
-        ""
-      )}
-    </Link>
+        {tabActive === "tv" || media.media_type === "tv" ? (
+          <div className="absolute right-2 top-[2%] inline-block rounded bg-white px-1 py-1 text-[1vw] font-bold text-black shadow-lg">
+            <p>TV SHOW</p>
+          </div>
+        ) : (
+          ""
+        )}
+      </Link>
+    </div>
   );
 };
 
